599aecd83ae06ec37df66d041f31ebda7b58eaa2
Rafael Karczevski <rkarczevski@gmail.com>
2023-02-24 10:05:56 -0400

    t: :sparkles: Melhorias e implementacoes gerais

---
+++ assets/konsole/ant-dark-konsole-zsh.profile
@@ -0,0 +1,8 @@
+[Appearance]
+ColorScheme=Ant-Dark
+Font=FiraCode Nerd Font Mono,11,-1,5,53,0,0,0,0,0,Retina
+
+[General]
+Command=/bin/zsh
+Name=ant-dark-konsole
+Parent=FALLBACK/
--- configure.sh
+++ configure.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
-sudo pacman -Sy git ansible --noconfirm
+sudo pacman -Sy git ansible yay --noconfirm
 
 ansible-playbook ansible/initial-configure.yaml
+
+ansible-playbook ansible/customization.yaml
+
+sudo cp -r assets/sddm/Ant-Dark /usr/share/sddm/themes
